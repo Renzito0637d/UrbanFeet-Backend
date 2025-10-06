b222b136f0eee60e4cdb13648008a4d70a825492
renzito0637d <renzito0637d@gmail.com>
2025-10-06 18:54:01 -0500

    feat: creacion de archivos de spring

---
+++ src/main/java/com/urbanfeet_backend/UrbanfeetApplication.java
@@ -0,0 +1,13 @@
+package com.urbanfeet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanfeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanfeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbanfeet_backend/UrbanfeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanfeet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanfeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
