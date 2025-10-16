c9afc5b5db9f9987ae022ff21fb688df8caed027
renzito0637d <renzito0637d@gmail.com>
2025-10-16 03:15:19 -0500

    feat: DAO en venta

--- src/main/java/com/urbanfeet_backend/DAO/Implements/VentaDAOImpl.java
+++ src/main/java/com/urbanfeet_backend/DAO/Implements/VentaDAOImpl.java
@@ -1,5 +1,41 @@
 package com.urbanfeet_backend.DAO.Implements;
 
-public class VentaDAOImpl {
-    
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.urbanfeet_backend.DAO.Interfaces.VentaDAO;
+import com.urbanfeet_backend.Entity.Venta;
+import com.urbanfeet_backend.Repository.VentaRepository;
+
+public class VentaDAOImpl implements VentaDAO {
+
+    @Autowired
+    private VentaRepository ventaRepository;
+
+    @Override
+    public List<Venta> findAll() {
+        return ventaRepository.findAll();
+    }
+
+    @Override
+    public void save(Venta venta) {
+        ventaRepository.save(venta);
+    }
+
+    @Override
+    public Venta findById(Integer id) {
+        return ventaRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void update(Venta venta) {
+        ventaRepository.save(venta);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        ventaRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/urbanfeet_backend/DAO/Interfaces/VentaDAO.java
+++ src/main/java/com/urbanfeet_backend/DAO/Interfaces/VentaDAO.java
@@ -1,5 +1,18 @@
 package com.urbanfeet_backend.DAO.Interfaces;
 
+import java.util.List;
+
+import com.urbanfeet_backend.Entity.Venta;
+
 public interface VentaDAO {
-    
+
+    public List<Venta> findAll();
+
+    public void save(Venta venta);
+
+    public Venta findById(Integer id);
+
+    public void update(Venta venta);
+
+    public void deleteById(Integer id);
 }
