3b4205c2afe0ca566f234eddf492836ee0b2b049
Angel <luang1007@gmail.com>
2025-10-16 02:52:14 -0500

    feat:Anotación @Repository e implementación de JpaRepository

--- src/main/java/com/urbanfeet_backend/Repository/DireccionRepository.java
+++ src/main/java/com/urbanfeet_backend/Repository/DireccionRepository.java
@@ -1,5 +1,9 @@
 package com.urbanfeet_backend.Repository;
 
-public class DireccionRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.urbanfeet_backend.Entity.Direccion;
+
+@Repository
+public interface DireccionRepository extends JpaRepository<Direccion, Integer> {
 }
