bc182ce0dc0a052704ee49b38b772791524b7a1a
renzito0637d <renzito0637d@gmail.com>
2025-10-16 02:44:45 -0500

    feat: uso de DAO en user

--- src/main/java/com/urbanfeet_backend/DAO/Implements/UserDAOImpl.java
+++ src/main/java/com/urbanfeet_backend/DAO/Implements/UserDAOImpl.java
@@ -1,5 +1,56 @@
 package com.urbanfeet_backend.DAO.Implements;
 
-public class UserDAOImpl {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.urbanfeet_backend.DAO.Interfaces.UserDAO;
+import com.urbanfeet_backend.Entity.User;
+import com.urbanfeet_backend.Entity.Enum.DocumentType;
+import com.urbanfeet_backend.Repository.UserRepository;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<User> listarUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User obtenerUserPorId(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public void guardarUser(User User) {
+        userRepository.save(User);
+    }
+
+    @Override
+    public void actualizarUser(User User) {
+        userRepository.save(User);
+    }
+
+    @Override
+    public void eliminarUser(Long id) {
+        User usuarioObj = userRepository.findById(id).get();
+        userRepository.delete(usuarioObj);
+    }
+
+    @Override
+    public Optional<User> buscarUserPorCorreo(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+    @Override
+    public Optional<User> buscarPorNumeroDocumento(DocumentType documentType, String documentNumber) {
+        return userRepository.findByDocumentTypeAndDocumentNumber(documentType, documentNumber);
+    }
+
 }
--- src/main/java/com/urbanfeet_backend/DAO/Interfaces/UserDAO.java
+++ src/main/java/com/urbanfeet_backend/DAO/Interfaces/UserDAO.java
@@ -1,5 +1,23 @@
 package com.urbanfeet_backend.DAO.Interfaces;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.urbanfeet_backend.Entity.User;
+import com.urbanfeet_backend.Entity.Enum.DocumentType;
+
 public interface UserDAO {
+    public List<User> listarUsers();
+
+    public User obtenerUserPorId(Long id);
+
+    public void guardarUser(User User);
+
+    public void actualizarUser(User User);
+
+    public void eliminarUser(Long id);
+
+    public Optional<User> buscarUserPorCorreo(String email);
     
+    public Optional<User> buscarPorNumeroDocumento(DocumentType documentType, String documentNumber);
 }
