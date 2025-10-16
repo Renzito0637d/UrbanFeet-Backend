ffc2acfea7b39ac1f6258dd873767e32ab52debc
renzito0637d <renzito0637d@gmail.com>
2025-10-16 02:45:01 -0500

    feat: uso de DAO en servicio

--- src/main/java/com/urbanfeet_backend/Services/Interfaces/UserService.java
+++ src/main/java/com/urbanfeet_backend/Services/Interfaces/UserService.java
@@ -1,11 +1,27 @@
 package com.urbanfeet_backend.Services.Interfaces;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.urbanfeet_backend.Entity.User;
+import com.urbanfeet_backend.Entity.Enum.DocumentType;
 import com.urbanfeet_backend.Model.AuthResponse;
 import com.urbanfeet_backend.Model.RegisterRequest;
 
 public interface UserService {
 
-    AuthResponse registerCliente(RegisterRequest request);
-    AuthResponse registerAdmin(RegisterRequest request);
-    
+    public List<User> listarUsers();
+
+    public User obtenerUserPorId(Long id);
+
+    public void guardarUser(User User);
+
+    public void actualizarUser(User User);
+
+    public void eliminarUser(Long id);
+
+    public AuthResponse registerCliente(RegisterRequest request);
+
+    public AuthResponse registerAdmin(RegisterRequest request);
+
 }
