7944004b38475f6ee67bc5043f8ae440ac531016
Renzito0637d <renzito0637d@gmail.com>
2025-12-06 02:45:55 -0500

    feat: metodos para el mantenimiento de los usuarios

--- src/main/java/com/urbanfeet_backend/DAO/Interfaces/UserDAO.java
+++ src/main/java/com/urbanfeet_backend/DAO/Interfaces/UserDAO.java
@@ -3,8 +3,13 @@ package com.urbanfeet_backend.DAO.Interfaces;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.urbanfeet_backend.Entity.User;
 import com.urbanfeet_backend.Entity.Enum.DocumentType;
+import com.urbanfeet_backend.Entity.Enum.RoleName;
+import com.urbanfeet_backend.Model.UserDTOs.UserResponse;
 
 public interface UserDAO {
     public List<User> listarUsers();
@@ -13,11 +18,16 @@ public interface UserDAO {
 
     public void guardarUser(User User);
 
-    public void actualizarUser(User User);
+    public User actualizarUser(User User);
 
     public void eliminarUser(Integer id);
 
     public Optional<User> buscarUserPorCorreo(String email);
     
     public Optional<User> buscarPorNumeroDocumento(DocumentType documentType, String documentNumber);
+
+    Page<UserResponse> listarPorRol(RoleName role, Pageable pageable);
+
+    Page<UserResponse> listarNoClientes(Pageable pageable);
+    
 }
