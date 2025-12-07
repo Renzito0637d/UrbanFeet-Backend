f24b98dadd8fd7928b60517abe0d7c62ca5c6021
Renzito0637d <renzito0637d@gmail.com>
2025-12-07 00:56:25 -0500

    feat: dto solo para cambiar la contraseña del user

---
+++ src/main/java/com/urbanfeet_backend/Model/UserDTOs/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.urbanfeet_backend.Model.UserDTOs;
+
+public class ChangePasswordRequest {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
