a665145b355df8f89d5f9a76d02a9d91ae1bc3e6
Renzito0637d <renzito0637d@gmail.com>
2025-12-07 19:04:47 -0500

    feat: controller zapatilla mejorada

--- src/main/java/com/urbanfeet_backend/Controller/ZapatillaController.java
+++ src/main/java/com/urbanfeet_backend/Controller/ZapatillaController.java
@@ -1,6 +1,7 @@
 package com.urbanfeet_backend.Controller;
 
 import com.urbanfeet_backend.Entity.Zapatilla;
+import com.urbanfeet_backend.Model.ZapatillaDTOs.ZapatillaResponse;
 import com.urbanfeet_backend.Services.Interfaces.ZapatillaService;
 
 import jakarta.validation.Valid;
@@ -22,22 +23,43 @@ public class ZapatillaController {
             String descripcion,
             String marca,
             String genero,
-            String tipo
-    ) {}
-
-   @PostMapping
-public ResponseEntity<?> crearZapatilla(@Valid @RequestBody ZapatillaRequest dto) {
-    Zapatilla creada = zapatillaService.crearZapatilla(dto);
-    return ResponseEntity.ok(creada);
-}
-
-@GetMapping("/{id}")
-public ResponseEntity<?> obtenerPorId(@PathVariable Integer id) {
-    Zapatilla zap = zapatillaService.obtenerZapatillaPorId(id);
-    if (zap == null) {
-        return ResponseEntity.notFound().build();
+            String tipo) {
     }
-    return ResponseEntity.ok(zap);
-}
-}
 
+    @PostMapping
+    public ResponseEntity<?> crearZapatilla(@Valid @RequestBody ZapatillaRequest dto) {
+        Zapatilla creada = zapatillaService.crearZapatilla(dto);
+        return ResponseEntity.ok(creada);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ZapatillaResponse> obtenerPorId(@PathVariable Integer id) {
+        ZapatillaResponse zapDto = zapatillaService.obtenerZapatillaPorId(id);
+
+        if (zapDto == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(zapDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ZapatillaResponse>> listarTodas() {
+        return ResponseEntity.ok(zapatillaService.obtenerTodo());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizarZapatilla(@PathVariable Integer id, @Valid @RequestBody ZapatillaRequest dto) {
+        Zapatilla actualizada = zapatillaService.actualizarZapatilla(id, dto);
+        return ResponseEntity.ok(actualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarZapatilla(@PathVariable Integer id) {
+        // Asumo que tu servicio tiene el método deleteZapatilla(id)
+        // Gracias al CascadeType.ALL que pusimos en la entidad,
+        // al borrar la zapatilla SE BORRARÁN TODAS SUS VARIACIONES automáticamente.
+        zapatillaService.eliminarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+}
