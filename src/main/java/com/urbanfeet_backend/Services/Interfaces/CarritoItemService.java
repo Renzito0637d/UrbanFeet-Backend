f0e6e782eea62acc0a4f51f06885c4a4f67e1f82
Renzito0637d <renzito0637d@gmail.com>
2025-12-08 20:38:43 -0500

    feat: nuevos metodo para ontener el carrito del user auth

--- src/main/java/com/urbanfeet_backend/Services/Interfaces/CarritoItemService.java
+++ src/main/java/com/urbanfeet_backend/Services/Interfaces/CarritoItemService.java
@@ -7,11 +7,12 @@ import org.springframework.security.core.Authentication;
 import com.urbanfeet_backend.Entity.Carrito;
 import com.urbanfeet_backend.Entity.Carrito_item;
 import com.urbanfeet_backend.Entity.Zapatilla_variacion;
-import com.urbanfeet_backend.Model.CarritoItemRequest;
-import com.urbanfeet_backend.Model.CarritoItemResponse;
+import com.urbanfeet_backend.Model.CarritoDTOs.CarritoItemRequest;
+import com.urbanfeet_backend.Model.CarritoDTOs.CarritoItemResponse;
+import com.urbanfeet_backend.Model.CarritoDTOs.CarritoResponse;
 
 public interface CarritoItemService {
-    
+
     public List<Carrito_item> obtenerTodo();
 
     public void guardar(Carrito_item carrito_item);
@@ -21,12 +22,12 @@ public interface CarritoItemService {
     public void actualizar(Carrito_item carrito_item);
 
     void eliminarPorId(Integer id, Authentication auth);
-    
+
     Carrito buscarCarritoPorId(Integer id);
 
     Zapatilla_variacion buscarVariacionPorId(Integer id);
 
-    Carrito obtenerCarritoDelUsuario(Authentication auth);
+    CarritoResponse obtenerCarritoDelUsuario(Authentication auth);
 
     CarritoItemResponse crearOIncrementarDesdeRequest(CarritoItemRequest request, Authentication auth);
 
